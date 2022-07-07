3d90b01e450a5502dc4bb33f2f16a0d4bd179cdb
wukaiwei1 <2596862515@qq.com>
2022-07-07 19:18:06 +0800

    完成自定义指令案例

---
+++ src/components/userDirectives.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{ str }}<br>
+        <input type="text" v-model="str" v-numder="str">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            str: ''
+        }
+    },
+    directives: {
+        numder: {
+            inserted(el, binding, vnode) {
+                vnode.context[binding.expression] = el.value.replace(/\D+/g, '')
+            },
+            update(el, binding, vnode) {
+                vnode.context[binding.expression] = el.value.replace(/\D+/g, '')
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
