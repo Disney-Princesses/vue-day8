ccda8adcbb45c239e90fe33e106f0825fab48f05
limengting <1443848358@qq.com>
2022-07-07 21:24:48 +0800

    商品表格-tags徽标

--- src/views/MyGoodsList.vue
+++ src/views/MyGoodsList.vue
@@ -2,23 +2,29 @@
   <div>
     <MyTable :glist="list">
       <template #header>
-      <th>#</th>
-      <th>商品名称</th>
-      <th>价格</th>
-      <th>标签</th>
-      <th>操作</th>
-    </template>
-    <template #tbody="scope">
-      <td>{{scope.row.id}}</td>
-      <td>{{scope.row.goods_name}}</td>
-      <td>{{scope.row.goods_price}}</td>
-      <td>{{scope.row.tags}}</td>
-      <td>
-        <button class="btn btn-danger btn-sm">删除</button>
-      </td>
-    </template>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <template #tbody="scope">
+        <td>{{scope.row.id}}</td>
+        <td>{{scope.row.goods_name}}</td>
+        <td>{{scope.row.goods_price}}</td>
+        <td>
+          <span
+            class="badge badge-warning"
+            v-for="(item,index) in scope.row.tags"
+            :key="index"
+            style="margin-right:10px"
+          >{{item}}</span>
+        </td>
+        <td>
+          <button class="btn btn-danger btn-sm">删除</button>
+        </td>
+      </template>
     </MyTable>
-    
   </div>
 </template>
 
